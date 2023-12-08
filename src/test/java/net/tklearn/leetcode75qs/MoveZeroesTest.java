38477ba4b7ea44be559ea1b1705250d7ea0cad86
tkttai <trankhactaia1@gmail.com>
2023-12-08 09:22:47 +0700

    #leetcode improved movezeroes solution

--- src/test/java/net/tklearn/leetcode75qs/MoveZeroesTest.java
+++ src/test/java/net/tklearn/leetcode75qs/MoveZeroesTest.java
@@ -3,6 +3,7 @@ package net.tklearn.leetcode75qs;
 import org.junit.jupiter.api.Test;
 
 import static net.tklearn.leetcode75qs.MoveZeroes.moveZeroes;
+import static net.tklearn.leetcode75qs.MoveZeroes.moveZeroes2;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MoveZeroesTest {
@@ -17,6 +18,18 @@ public class MoveZeroesTest {
     public void test2() {
         int[] arr = {1};
         moveZeroes(arr);
+        assertArrayEquals(new int[]{1}, arr);
+    }
+    @Test
+    public void test3() {
+        int[] arr = {0, 1, 0, 3, 12};
+        moveZeroes2(arr);
+        assertArrayEquals(new int[]{1, 3, 12, 0, 0}, arr);
+    }
+    @Test
+    public void test4() {
+        int[] arr = {1};
+        moveZeroes2(arr);
         assertArrayEquals(new int[]{1}, arr);
     }
 }
